91ee9c217b339f0e00def59cbf3000549dd1b4fb
Rix <i@rix.li>
2015-06-14 19:16:13 +0800

    discard .jsx extension

--- app/assets/jsx/components/MainSection.react.jsx
+++ app/assets/jsx/components/MainSection.react.jsx
@@ -1,11 +1,11 @@
 const React = require('react');
-import {Body} from './Body.react.jsx';
+import {Body} from './Body.react';
 
 export const MainSection = React.createClass({
   render() {
     return (
-      <div>
-        <h1>Example of React with ES6 and Browserify</h1>
+      <div className="main">
+        <h1>React Blank Project</h1>
         <Body />
       </div>
     );
